91145623076ed89b55c234a89dc69cc4d12c52f5
Peter Östman <52030283+Kungtroll@users.noreply.github.com>
2019-11-20 12:47:32 +0100

    Bugfix
    Bugfix, $Server variable was not set in the default mode. Output file is now cleared beginning of each run. Default filepath was also changed, and formatting to comments.

--- Powershell_Tutorials/Lektion_10/Test-ConnectionRSPoolToFile.ps1
+++ Powershell_Tutorials/Lektion_10/Test-ConnectionRSPoolToFile.ps1
@@ -1,7 +1,7 @@
 Param(
     [Switch]$DoItFast,
     $MaxThreads = 5,
-    $FilePath = "C:\temp\PingResultRunspacePool.txt"
+    $FilePath = "C:\temp\PingResult.txt"
     )
 
 $Servers =@(
@@ -51,10 +51,11 @@ If ($DoItFast){
         #By using a runspacepool, we can ping as many servers simultaneoulsy as 
         #the maximum number of threads we allow. Increasing the number of threads
         #can reduce the time it takes to complete all the commands, however it will
-        #consume more resources on the host running the script. Playing with 
-        #maximum threads parameter can show where the sweet-spot is for the number
-        #of threads, there will be a spot where we get no more reduction in time to
-        #complete the commands. 
+        #consume more resources on the host running the script. 
+        
+        #Playing with maximum threads parameter can show where the sweet-spot is for 
+        #the number of threads, there will be a spot where we get no more reduction 
+        #in time to complete the commands. 
         #You can measure this with the measure-command cmdlet.
 
         $PowerShell = [powershell]::Create()
@@ -89,7 +90,9 @@ If ($DoItFast){
     $RunspacePool.Close()
 } #End If
 else {
-    $Servers | Foreach-Object {
+    $Servers |
+    Foreach-Object {
+        $Server = $_
         $OutObject =  [pscustomobject]@{
             ServerName = $_
             Connectable = $false
