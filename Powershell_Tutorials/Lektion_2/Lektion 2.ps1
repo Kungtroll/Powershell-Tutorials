f45f64231a59f57d44324de98bbeaec2e757e67b
Peter Östman <52030283+Kungtroll@users.noreply.github.com>
2020-01-29 14:47:20 +0100

    Translation and new contect to Lektion 2

--- Powershell_Tutorials/Lektion_2/Lektion 2.ps1
+++ Powershell_Tutorials/Lektion_2/Lektion 2.ps1
@@ -1,36 +1,60 @@
 ﻿<# 
 
-Repetition från Lektion 1:
+Recap from session 1:
 
-Datyper: Int, string, boolean, single, datetime, xml, array, hashtable
+Datypes: Int, string, boolean, single, datetime, xml, array, hashtable
 
-Objekt:
-Object - Methods & Properties
+Objects - Methods & Properties:
 $objectname.methodname
 $objectname.propertyname
 $objectname.gettype()
 $objectname | get-member
 
-Stränghantering
-
+String management
 ForEach
 
 #>
 
 $Numbers = @(1,2,3,4,5,6,7,8,9,0)
-write-host "Element 0 är" $Numbers[0] -ForegroundColor Green
+write-host "Element 0 is" $Numbers[0] -ForegroundColor Green
 
-Foreach ($number in $numbers)
+# The simplest way of looping through the array is to use a 
+# simple ForEach
+
+ForEach ($number in $numbers)
     {
     write-host $number -ForegroundColor Green
     }
 
 
-#I pipeline
+# In the pipeline
 $Numbers | %($_){write-host $_ -ForegroundColor Green}
-#Or without % alias
+# '%' is an alias of 'ForEach-Object'. Alias can introduce possible
+# problems and make scripts hard to maintain. Please consider 
+# changing alias to its full content.
+
+#So without the % alias
 $Numbers | ForEach-Object ($_){write-host $_ -ForegroundColor Green}
-#You can also use the foreach method on the array-object
+# ForEach-Object is a cmdlet with a bunch of parameters that 
+# offers more functionality than ForEach.
+# Foreach is also an alias for Foreach-Object, meaning that
+# these two is easily confused. A way of telling is that 
+# ForEach is followed by the ""$itemvariable in $datasetvariable)"
+# Examples:
+# ForEach ($item in $items) {Do something}
+# ForEach($stone in $bucket) {"Throw away stone"}
+
+#This one is actually when you use the alias for ForEach-Object
+$Numbers |ForEach {write-host $_ -ForegroundColor Green}
+# Visual Studio code remarks a problem here:
+# 'ForEach' is an alias of 'ForEach-Object'. 
+# Alias can introduce possible problems and make scripts hard
+# to maintain. Please consider changing alias to its full content.
+
+
+# You can also use the foreach method on the array-object
+# This was introduced in POSH V4 and is supposed to be 
+# considerably faster when working with large datasets.
 $Numbers.ForEach({write-host $_ -ForegroundColor Green})
 
 <#
@@ -44,51 +68,51 @@ Select-Object -Expandproperty
 
 
 <#
-Lektion 2
+Session 2
 
-Gå igenom uppgiften från Lektion 1
+Walkthrough of the exercise from session 1
 If, ElseIf, Else
 Switch
 Do..While
 Read-Host
 Write-Host
 
-Uppgift till nästa gång
+Exercise until next session
 
-(Funktioner)
+(Functions)
 #>
 
-$Tal = Read-Host "Skriv ett Tal"
+$Number = Read-Host "Enter a number"
 
-If ($Tal -lt 10) 
+If ($Number -lt 10) 
     {
-    Write-Host "Talet är mindre än 10"
+    Write-Host "The number is less than 10"
     }
-    ElseIf ($Tal -eq 10)
+    ElseIf ($Number -eq 10)
     {
-    Write-Host "Talet är 10"
+    Write-Host "The number is 10"
     }
     Else
     {
-    Write-Host "Talet är större än 10"
+    Write-Host "The number is greater than 10"
     }
 
 #Switch
 
-Switch (Read-Host "Skriv ett Tal mellan 1-5")
+Switch (Read-Host "Enter a number between 1-5")
     {
-       1 {Write-Host "Talet är 1"}
+       1 {Write-Host "The number is 1"}
     
-       2 {Write-Host "Talet är 2"}
+       2 {Write-Host "The number is 2"}
 
-       3 {Write-Host "Talet är 3"}
+       3 {Write-Host "The number is 3"}
 
-       4 {Write-Host "Talet är 4"}
+       4 {Write-Host "The number is 4"}
 
-       5 {Write-Host "Talet är 5"}
+       5 {Write-Host "The number is 5"}
        default 
        {
-       Write-Host "Talet ligger inte mellan 1-5" ;Break
+       Write-Host "The number is not within 1-5" ;Break
        }
     }
 
@@ -104,40 +128,39 @@ DO
     } 
     While ($a -lt 5)
 
-#Eller en do-while med en if-else inuti
+#Or a do-while with an if-else inside
 
 $Sant = $False
 Do 
 
 {
-    $Answer = Read-Host "Vilken är Mexicos Huvudstad?"
+    $Answer = Read-Host "What is the capital of Mexico?"
     If ($Answer -eq "Mexico City") 
         {
         $Sant = $True
-        write-Host "Rätt Svar!"
+        write-Host "Correct!"
         }
     Else 
         {
         $Sant = $False
-        write-host "Fel svar, försök igen!"
+        write-host "Wrong answer, please try again!"
         }
 }
 
 While ($Sant -ne $True)
 
-
-# Funktioner
-# En funktion är egentligen samma sak som ett objekts metod.
-# Funktionen kan ta emot inparametrar och kan returnera information
-# Funktioner begränsar "scope" för variabler, en variabel som används i en funktion
-# är inte tillgänglig utanför funktionen.
-# Vi nosar bara lite på detta idag, då det finns ganska mycket mer att veta om funktioner.
-
+# Functions
+# A function is basically the same thing as a method of an object
+# The function can receive input parameters and return information
+# Functions limits the "scope" of variables, a variable used in a 
+# function is not available outside the function.
+# This is just a brief introduction to functions, there is a lot
+# more to know about functions.
 Function WriteMyName ($name)
     {
-        $Halsning = "Hej " + $Name
-        Return $Halsning
+        $Greeting = "Hello " + $Name
+        Return $Greeting
     }
 
-$Myname = read-host "Vad heter du?"
+$Myname = read-host "Please enter your name..."
 write-host -ForegroundColor Green (WriteMyName $Myname)
