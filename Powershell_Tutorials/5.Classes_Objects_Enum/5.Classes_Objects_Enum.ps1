e9c08eab84d37c1b038586632ffff1902a9ffee1
Peter Östman <52030283+Kungtroll@users.noreply.github.com>
2020-03-19 14:43:16 +0100

    Update 5.Classes_Objects_Enum.ps1

--- Powershell_Tutorials/5.Classes_Objects_Enum/5.Classes_Objects_Enum.ps1
+++ Powershell_Tutorials/5.Classes_Objects_Enum/5.Classes_Objects_Enum.ps1
@@ -19,6 +19,33 @@ Enum
 
 $MittObjekt = New-Object -TypeName PSCustomObject
 
+$MittObjekt | Get-Member
+
+#Nu fick vi en type (TypeName) - det vill säga klass. 
+#Det är en .Net klass, Powershell bygger på .Net, precis som språk som:
+# C#, J#, F#, VB.Net., C++/CLI, Managed C++, IronPython etc...
+#Språken har alltså mycket gemensamt vilket gör det lättare för oss att
+#vid behov lära oss nästa språk i .Net familjen.
+
+[System.Management.Auto] #Ctrl + Space
+#Nu kan vi browsa i klasserna
+
+[System.Management.Automation.PSCustomObject]
+#Lägg till :: så kan du se metoder.
+[System.Management.Automation.PSCustomObject]::Equals #Enter
+#Constructor: static bool Equals(System.Object objA, System.Object objB)
+#Vad betyder detta?
+#Metoden returnerar en static bool, d.v.s. en True Eller False
+#Input för metoden är System.Object objA, objB
+#Sammanfattat:
+#Den kan alltså jämföra objA med ObjB och returnera en
+#True eller False.
+
+
+
+#Nog om .Net, tillbaka till powershell
+#och PScustom objektet vi skapade - $MittObjekt
+
 #För att lägga till properties till objektet använder vi Add-Member 
 # Ni minns väl att vi använde Get-Member för att titta på vilka methods & Properties ett objekt har.
 
@@ -40,28 +67,27 @@ $MinArrayAvObjekt = @()
 $MinArrayAvObjekt += $MittObjekt
 $MinArrayAvObjekt += $MittAndraObjekt
 
-ForEach ($Objekt in $MinArrayAvObjekt)
-    {
-        write-host $Objekt.Text -ForegroundColor $Objekt.Color
-    }
+ForEach ($Objekt in $MinArrayAvObjekt){
+    write-host $Objekt.Text -ForegroundColor $Objekt.Color
+}
 #På samma sätt kan jag bygga en ny Array av Objekt med värden från andra objekt
 
 $MinAndraArrayAvObjekt = @()
-ForEach ($Objekt in $MinArrayAvObjekt)
-    {
-        $Objekt2 = New-Object -TypeName PSCustomObject
-        $Objekt2 | Add-member -MemberType NoteProperty -Name "Text" -Value $Objekt.Text
-        $Objekt2 | Add-member -MemberType NoteProperty -Name "Color" -Value $Objekt.Color
-        $Objekt2 | Add-member -MemberType NoteProperty -Name "NyProperty" -Value "Ett värde från något annat objekt"
-        $MinAndraArrayAvObjekt += $Objekt2
-    }
+ForEach ($Objekt in $MinArrayAvObjekt){
+    $Objekt2 = New-Object -TypeName PSCustomObject
+    $Objekt2 | Add-member -MemberType NoteProperty -Name "Text" -Value $Objekt.Text
+    $Objekt2 | Add-member -MemberType NoteProperty -Name "Color" -Value $Objekt.Color
+    $Objekt2 | Add-member -MemberType NoteProperty -Name "NyProperty" -Value "Ett värde från något annat objekt"
+    $MinAndraArrayAvObjekt += $Objekt2
+}
 $MinAndraArrayAvObjekt
 
 #Skapa en egen klass
 
 #Man kan skapa Custom property types istället för String, Int, etc.
-Enum Color
-{
+#De kallas för Enum - eller enumerator och låter oss specificera
+#en lista över vilka värden som är giltiga i den propertyn.
+Enum Color{
     Red
     Green
     Blue
@@ -71,8 +97,8 @@ Enum Color
 }
 
 
-Class Car
-{
+Class Car{
+    #Properties
     [String]$PlateNumber
     static [String]$Name = 'Gamla bettan'
     [int]$numberOfDoors
@@ -82,8 +108,7 @@ Class Car
     [String]$Direction
 
     #Skapa en metod för klassen
-    [void]TurnCar ([String]$Arg) 
-    {
+    [void]TurnCar ([String]$Arg){
         $This.Direction = $Arg
     }
 }
@@ -101,25 +126,33 @@ $MinVolvo.Color = 'Red'
 $MinVolvo.Direction = 'Forward'
 $MinVolvo
 
+#Vilken riktning åker bilen?
+$MinVolvo.Direction
+#Sväng vänster
 $MinVolvo.TurnCar("Left")
-$MinVolvo
+#Vilken riktning åker bilen nu?
+$MinVolvo.Direction
+
+#Vi satte namnet till en static, ett värde som aldrig ändras
+#En static kan man se på följande sätt 
 $MinVolvo::Name
 
+#Static används t.ex. på metoder som man inte behöver instatiera
+#för att kunna använda. T.ex. Math
+[System.Math]::Floor(3.14)
+#Metoden gör uträkningen åt oss utan att vi behöver skapa ett Math objekt. 
+
 # Felhantering
-
-Function Get-User ($User)
-{
-
-    Try 
-    {
+Function Get-User ($User){
+    Try{
         $ObjUser = Get-ADUser $User -ErrorAction Stop
-    }
-
-    Catch
-    {
-        $Error
+    }Catch{
+        #$Error
         Write-host "Kunde inte hitta kontot"
+        Throw "Exiting..."
     }
 
     Return $objUser
 }
+
+Get-User -User "Kalle"
